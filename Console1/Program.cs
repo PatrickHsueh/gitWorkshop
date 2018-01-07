1b297065462b726176ccdb30786a9300ff1a454b
Patrick <patrick@gbrco.net>
2018-01-08 07:53:18 +0800

    added C# sample.

---
+++ Console1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test();
+        }
+
+        public static void test()
+        {
+            String directory = @"g:\xx\";
+            String[] linesOld = File.ReadAllLines(Path.Combine(directory, "PDC1_old.csv"));
+            String[] linesNew = File.ReadAllLines(Path.Combine(directory, "PDC1.csv"));
+
+            IEnumerable<String> onlyNew = linesNew.Except(linesOld);
+
+            File.WriteAllLines(Path.Combine(directory, "Result.txt"), onlyNew, Encoding.ASCII);
+        }
+    }
+}
