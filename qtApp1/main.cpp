d741d787b4272693040dc0fe4dbbf283f746407b
Patrick <patrick@gbrco.net>
2018-01-24 22:04:18 +0800

    qt deploy issue fixed.

--- qtApp1/main.cpp
+++ qtApp1/main.cpp
@@ -1,10 +1,29 @@
 #include "qtapp1.h"
 #include <QtWidgets/QApplication>
 
+
+void test();
+
+
 int main(int argc, char *argv[])
 {
+	test();
+
 	QApplication a(argc, argv);
 	qtApp1 w;
 	w.show();
 	return a.exec();
+}
+
+void test()
+{
+	char s[256] = "\0";
+	scanf(s, "Input string:");
+
+	char *t = s;
+	t[0] = 'D';
+
+	printf("s is now %s:", s);
+	
+	return;
 }
---
+++ qtApp1/readme.txt
@@ -0,0 +1,6 @@
+Must Do it:
+Copy folder from
+d:\QT\5.3.2\5.3\msvc2012_opengl\plugins\platforms\
+
+To
+d:\__gitWorkshop\qtApp1\Win32\Debug\platforms\
